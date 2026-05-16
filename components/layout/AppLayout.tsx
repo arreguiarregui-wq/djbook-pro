27a90ae3c8ca0cd7ae7483d7ee72be328f115d1b
arreguiarregui-wq <arreguiarregui@gmail.com>
2026-05-17 00:57:47 +0200

    Update AppLayout.tsx

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -1,13 +1,15 @@
 'use client'
+
 import Sidebar from '@/components/layout/Sidebar'
 import { useAuth } from '@/lib/useAuth'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   useAuth()
+
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="ml-[220px] flex-1 p-8 min-h-screen">
+      <main className="md:ml-[220px] flex-1 p-4 md:p-8 min-h-screen pt-14 md:pt-8 pb-20 md:pb-8">
         {children}
       </main>
     </div>

7e68ebf37c0e9f9e8bcf92855e25454153419ee8
arreguiarregui-wq <arreguiarregui@gmail.com>
2026-05-23 01:23:27 +0200

    Update AppLayout.tsx

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Sidebar from '@/components/layout/Sidebar'
+import HoloBlob from '@/components/ui/HoloBlob'
 import { useAuth } from '@/lib/useAuth'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
@@ -8,6 +9,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className="flex min-h-screen">
+      <HoloBlob />
       <Sidebar />
       <main className="md:ml-[220px] flex-1 p-4 md:p-8 min-h-screen pt-14 md:pt-8 pb-20 md:pb-8">
         {children}

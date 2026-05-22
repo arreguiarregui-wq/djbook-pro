cbc36afdbf287ed55c88b6f5a8d7626145a43f20
arreguiarregui-wq <arreguiarregui@gmail.com>
2026-05-23 01:37:52 +0200

    Update AppLayout.tsx

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -8,7 +8,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   useAuth()
 
   return (
-    <div className="flex min-h-screen">
+  <div className="flex min-h-screen" style={{ background: 'transparent' }}>
       <HoloBlob />
       <Sidebar />
       <main className="md:ml-[220px] flex-1 p-4 md:p-8 min-h-screen pt-14 md:pt-8 pb-20 md:pb-8">
